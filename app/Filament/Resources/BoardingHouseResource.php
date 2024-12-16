7532e142daa6f7a170696273c1017e7d4a4acd9f
penhele <stephenhelk@gmail.com>
2024-12-17 00:54:13 +0700

    CRUD Kos (Testing Data)

--- app/Filament/Resources/BoardingHouseResource.php
+++ app/Filament/Resources/BoardingHouseResource.php
@@ -113,13 +113,19 @@ class BoardingHouseResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('city.name'),
+                Tables\Columns\TextColumn::make('category.name'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\ImageColumn::make('thumbnail'),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
