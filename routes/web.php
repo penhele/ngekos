5094253bb3ec16fb62afd59850eae2d059d5c23f
penhele <stephenhelk@gmail.com>
2024-12-17 23:27:12 +0700

    Menampilkan Kos Berdasarkan Kategori

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+
+Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
 
 Route::get('/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
 Route::get('/find-results', [BoardingHouseController::class, 'findResults'])->name('find-kos.results');
