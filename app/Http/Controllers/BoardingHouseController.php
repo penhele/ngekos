76f13efac0bedeaead235e8c4231c3ca63782077
penhele <stephenhelk@gmail.com>
2024-12-19 00:42:48 +0700

    Halaman Kamar Kos

--- app/Http/Controllers/BoardingHouseController.php
+++ app/Http/Controllers/BoardingHouseController.php
@@ -29,6 +29,12 @@ class BoardingHouseController extends Controller
         return view('pages.boarding-house.show', compact('boardingHouse'));
     }
 
+    public function rooms($slug)
+    {
+        $boardingHouse = $this->boardingHouseRepository->getBoardingHouseBySlug($slug);
+        return view('pages.boarding-house.rooms', compact('boardingHouse'));
+    }
+
     public function find()
     {
         $categories = $this->categoryRepository->getAllCategories();
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/city/{slug}', [CityController::class, 'show'])->name('city.show');
 Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
 
 Route::get('/kos/{slug}', [BoardingHouseController::class, 'show'])->name('kos.show');
+Route::get('/kos/{slug}/rooms', [BoardingHouseController::class, 'rooms'])->name('kos.rooms');
 
 Route::get('/find-kos', [BoardingHouseController::class, 'find'])->name('find-kos');
 Route::get('/find-results', [BoardingHouseController::class, 'findResults'])->name('find-kos.results');
